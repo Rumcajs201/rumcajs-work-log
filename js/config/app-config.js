ea66c27a65dcb56b5866ef4a8c8f7a5ec6dc945b
Rumcajs201 <95769038+Rumcajs201@users.noreply.github.com>
2026-07-22 19:49:36 +0200

    Bump version to 0.6.2 after work button fix

--- js/config/app-config.js
+++ js/config/app-config.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = Object.freeze({
-  version: "0.6.1",
+  version: "0.6.2",
   availableProfiles: ["europris", "universal"],
   defaultProfile: "europris",
   allowProfileChange: true,
