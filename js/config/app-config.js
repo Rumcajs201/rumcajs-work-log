a10b4f1c1b804ee092cd10009ec17504f9860725
Rumcajs201 <95769038+Rumcajs201@users.noreply.github.com>
2026-07-22 10:37:26 +0200

    Dodaj konfigurację profili i wersji aplikacji

---
+++ js/config/app-config.js
@@ -0,0 +1,14 @@
+export const APP_CONFIG = Object.freeze({
+  version: "0.3.0",
+  availableProfiles: ["europris", "universal"],
+  defaultProfile: "europris",
+  allowProfileChange: true,
+  availableLanguages: ["pl", "en", "de", "no"],
+  defaultLanguage: "pl",
+  contactEmail: "rumcajs.worklog@gmail.com"
+});
+
+export const PROFILE_LABELS = Object.freeze({
+  europris: "Europris",
+  universal: "Uniwersalny"
+});
