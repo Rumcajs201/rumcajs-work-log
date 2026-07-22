4e974b1e5ee1ace06e2085cd383f497f2c89e6ec
Rumcajs201 <95769038+Rumcajs201@users.noreply.github.com>
2026-07-22 11:01:40 +0200

    Ustaw wersję 0.4.0

--- js/config/app-config.js
+++ js/config/app-config.js
@@ -1,14 +1,9 @@
 export const APP_CONFIG = Object.freeze({
-  version: "0.3.0",
+  version: "0.4.0",
   availableProfiles: ["europris", "universal"],
   defaultProfile: "europris",
   allowProfileChange: true,
   availableLanguages: ["pl", "en", "de", "no"],
   defaultLanguage: "pl",
   contactEmail: "rumcajs.worklog@gmail.com"
-});
-
-export const PROFILE_LABELS = Object.freeze({
-  europris: "Europris",
-  universal: "Uniwersalny"
 });
