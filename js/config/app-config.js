b929dfb71bf0622aa8f6a90ecd5063b4be8fffd5
Rumcajs201 <95769038+Rumcajs201@users.noreply.github.com>
2026-07-22 13:58:40 +0200

    Bump version for carrier selection and operation editing

--- js/config/app-config.js
+++ js/config/app-config.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = Object.freeze({
-  version: "0.5.4",
+  version: "0.5.5",
   availableProfiles: ["europris", "universal"],
   defaultProfile: "europris",
   allowProfileChange: true,
