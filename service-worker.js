51bd4a7e206719a39c4473352b1e85b933761cde
Rumcajs201 <95769038+Rumcajs201@users.noreply.github.com>
2026-07-23 06:22:45 +0200

    Refresh cache for v0.8.1

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "rumcajs-work-log-v0.8.0-trailer-odometer-1";
+const CACHE = "rumcajs-work-log-v0.8.1-single-event-form-1";
 const ASSETS = [
   "./","./index.html","./manifest.webmanifest","./css/app.css","./css/saved-places.css","./css/carrier-context.css","./css/fleet-ui.css","./css/operation-editor.css","./css/vehicle-day.css",
   "./js/app.js","./js/i18n.js","./js/language-switcher.js","./js/history-search.js","./js/carrier-context.js","./js/fleet-ui.js","./js/operation-editor.js","./js/payments-settings.js","./js/trailer-events.js","./js/workday-address.js","./js/vehicle-day.js","./js/config/app-config.js","./js/db/indexeddb.js","./js/modules/time.js","./js/modules/workdays.js","./js/modules/operations.js","./js/modules/backup.js","./js/modules/gps.js","./js/modules/stores.js","./js/modules/reverse-geocode.js","./js/modules/places.js","./data/carriers.json","./data/hansen-jensen-halden-fleet.json"
