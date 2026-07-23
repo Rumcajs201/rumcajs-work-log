9700cc59e5481d4356797ddef82b26c81913edc2
Rumcajs201 <95769038+Rumcajs201@users.noreply.github.com>
2026-07-23 08:47:24 +0200

    Refresh PWA cache for v0.9.5

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "rumcajs-work-log-v0.9.4-controls-repair-1";
+const CACHE = "rumcajs-work-log-v0.9.5-workday-state-fix-1";
 const ASSETS = [
   "./","./index.html","./manifest.webmanifest","./assets/rumcajs-mark.svg","./css/app.css","./css/saved-places.css","./css/carrier-context.css","./css/fleet-ui.css","./css/operation-editor.css","./css/vehicle-day.css","./css/dashboard.css",
   "./js/app.js","./js/i18n.js","./js/language-switcher.js","./js/history-search.js","./js/carrier-context.js","./js/fleet-ui.js","./js/operation-editor.js","./js/payments-settings.js","./js/trailer-events.js","./js/workday-address.js","./js/vehicle-day.js","./js/dashboard.js","./js/operation-controls-fix.js","./js/config/app-config.js","./js/db/indexeddb.js","./js/modules/time.js","./js/modules/workdays.js","./js/modules/operations.js","./js/modules/backup.js","./js/modules/gps.js","./js/modules/stores.js","./js/modules/reverse-geocode.js","./js/modules/places.js","./data/carriers.json","./data/hansen-jensen-halden-fleet.json"
