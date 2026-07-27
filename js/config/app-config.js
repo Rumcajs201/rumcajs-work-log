f6f3672c18e751d85e8fdb8d9d4249deb59a7e23
Rumcajs201 <95769038+Rumcajs201@users.noreply.github.com>
2026-07-27 09:15:50 +0200

    Bump version for active workday layout

--- js/config/app-config.js
+++ js/config/app-config.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = Object.freeze({
-  version: "0.12.0",
+  version: "0.12.1",
   availableProfiles: ["europris", "universal"],
   defaultProfile: "europris",
   allowProfileChange: true,
