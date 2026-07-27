ef7d7625ac994bb92bc514c5cd153ef0a7c61ce1
Rumcajs201 <95769038+Rumcajs201@users.noreply.github.com>
2026-07-27 15:40:31 +0200

    Release v0.14.0 feedback workflow

--- js/config/app-config.js
+++ js/config/app-config.js
@@ -1,10 +1,11 @@
 export const APP_CONFIG = Object.freeze({
-  version: "0.13.1",
+  version: "0.14.0",
   availableProfiles: ["europris", "universal"],
   defaultProfile: "europris",
   allowProfileChange: true,
   defaultCarrierByProfile: { europris: "hansen-jensen-halden" },
   availableLanguages: ["pl", "en", "de", "no"],
   defaultLanguage: "pl",
-  contactEmail: "rumcajs.worklog@gmail.com"
+  contactEmail: "rumcajs.worklog@gmail.com",
+  feedbackSubjectPrefixes: ["BUG", "POMYSŁ", "PYTANIE"]
 });
