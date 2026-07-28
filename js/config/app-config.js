c9a8cd8ff0f6c85329c373300ced4ebd7ce73ac5
Rumcajs201 <95769038+Rumcajs201@users.noreply.github.com>
2026-07-28 07:31:14 +0200

    Release v0.16.0 transport and data health

--- js/config/app-config.js
+++ js/config/app-config.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = Object.freeze({
-  version: "0.15.1",
+  version: "0.16.0",
   availableProfiles: ["europris", "universal"],
   defaultProfile: "europris",
   allowProfileChange: true,
