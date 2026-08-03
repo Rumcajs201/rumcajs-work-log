657926297d3dc625d1d79a4e5c1a141f4ad38450
Rumcajs201 <95769038+Rumcajs201@users.noreply.github.com>
2026-08-03 10:13:39 +0200

    Bump version for compact reports and first-open fix

--- js/config/app-config.js
+++ js/config/app-config.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = Object.freeze({
-  version: "0.16.16",
+  version: "0.16.17",
   availableProfiles: ["europris", "universal"],
   defaultProfile: "europris",
   allowProfileChange: true,
