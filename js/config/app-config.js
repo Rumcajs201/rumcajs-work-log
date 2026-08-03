ccf79f4544afc28139285a2549d38af87661bb66
Rumcajs201 <95769038+Rumcajs201@users.noreply.github.com>
2026-08-03 07:30:25 +0200

    Bump app version for reports module

--- js/config/app-config.js
+++ js/config/app-config.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = Object.freeze({
-  version: "0.16.13",
+  version: "0.16.14",
   availableProfiles: ["europris", "universal"],
   defaultProfile: "europris",
   allowProfileChange: true,
