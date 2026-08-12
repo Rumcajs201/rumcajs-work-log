ddb444422b60320b3e8edad9fced795eada6c260
Rumcajs201 <95769038+Rumcajs201@users.noreply.github.com>
2026-08-12 13:06:51 +0200

    Bump version to 0.16.31

--- js/config/app-config.js
+++ js/config/app-config.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = Object.freeze({
-  version: "0.16.30",
+  version: "0.16.31",
   availableProfiles: ["europris", "universal"],
   defaultProfile: "europris",
   allowProfileChange: true,
